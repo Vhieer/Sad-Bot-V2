3d60ffe0ab124ae86ca313e85ba23d166f0cca8d
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-11 16:34:57 +0700

    Update menu.cjs

--- plugins/menu.cjs
+++ plugins/menu.cjs
@@ -406,7 +406,7 @@ var handler = async (m, {
 		}
 		text = await tiny(text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name]))
 		var pp = await conn.profilePictureUrl(conn.user.jid, 'image').catch(_ => './src/avatar_contact.png')
-		conn.sendHydrated(m.chat, text.trim(), 'Bot Papah-Chan', pp, 'https://github.com/FahriAdison', 'Github', null, null, [
+		conn.sendHydrated(m.chat, text.trim(), '𝕊𝕖𝕔𝕣𝕖𝕥 𝕄𝔻', pp, 'https://github.com/Vhieer', 'Github', null, null, [
 			['Donate', '/donasi'],
 			['Speed', '/ping'],
 			['Owner', '/owner']
